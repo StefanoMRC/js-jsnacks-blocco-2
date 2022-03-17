42fa9315975cbbb61ad816b8a9c254740475347f
StefanoMRC <stefano.mrc92@gmail.com>
2022-03-17 10:08:40 +0100

    correzione es 2

--- Snack2/main.js
+++ Snack2/main.js
@@ -1,8 +1,14 @@
 let listaNomi=["pippo", "dario", "dina", "giorgio"]
 let listaCognomi=["lampa", "lampa", "de giorgi", "franco"]
-let nomeCognome=""
-for(let i=0; i<listaCognomi.length; i++){
-nomeCognome= `${listaNomi[i]} ${listaCognomi[i]}`;
-console.log(nomeCognome)
-document.getElementById("cont").innerHTML+=`<li>${nomeCognome}</li>`
+let nomeCognome=[]
+// for(let i=0; i<listaCognomi.length; i++){
+// nomeCognome= `${listaNomi[i]} ${listaCognomi[i]}`;
+// console.log(nomeCognome)
+// document.getElementById("cont").innerHTML+=`<li>${nomeCognome}</li>`
+// }
+for(let i=0; i<8; i++){
+    let numRandomNomi= Math.floor(Math.random()*(listaNomi.length));
+    let numRandomCognomi= Math.floor(Math.random()*(listaCognomi.length));
+    nomeCognome= `${listaNomi[numRandomNomi]} ${listaCognomi[numRandomCognomi]}`;
+    document.getElementById("cont").innerHTML+=`<li>${nomeCognome}</li>`
 }
