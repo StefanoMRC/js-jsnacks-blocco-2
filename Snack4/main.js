0657aa97770f5488d7c19d484623544b251fb6a5
StefanoMRC <stefano.mrc92@gmail.com>
2022-03-17 10:26:39 +0100

    es.4 stampa a video

--- Snack4/main.js
+++ Snack4/main.js
@@ -2,9 +2,11 @@ let array=["1","2","3","4","5","6"]
 let array2=["1","2","3"]
 numArray=array.length
 numArray2=array2.length
-
+document.getElementById("cont").innerHTML=`${array}`
 
 for(i=0; i<(numArray-numArray2); i++){
     array2.push(Math.round(Math.random()*10 + 1))
 }
 console.log(array2)
+document.getElementById("cont2").innerHTML=`${array2}`
+
