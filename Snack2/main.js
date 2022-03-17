d5dcf7e2d048f4c17cd79e9a106e6039d39fc064
StefanoMRC <stefano.mrc92@gmail.com>
2022-03-17 10:21:29 +0100

    es2

--- Snack2/main.js
+++ Snack2/main.js
@@ -1,6 +1,6 @@
 let listaNomi=["pippo", "dario", "dina", "giorgio"]
 let listaCognomi=["lampa", "lampa", "de giorgi", "franco"]
-let nomeCognome=[]
+let nomeCognome=""
 // for(let i=0; i<listaCognomi.length; i++){
 // nomeCognome= `${listaNomi[i]} ${listaCognomi[i]}`;
 // console.log(nomeCognome)
